76183242f3938abc104a1fdc1bd8fd94aba501c1
Kristoffermc <kristoffermchristensen@gmail.com>
2017-04-27 14:25:41 +0200

    Created the AddVolunteer, AdminView and HoursView

---
+++ MyVolunteer/src/myvolunteer/GUI/Controller/AdminViewController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myvolunteer.GUI.Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Kristoffers
+ */
+public class AdminViewController implements Initializable
+{
+
+    @FXML
+    private ComboBox<?> comboBoxGuild;
+    @FXML
+    private ListView<?> volunteerList;
+    @FXML
+    private TextField searchBar;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        // TODO
+    }    
+    
+}
