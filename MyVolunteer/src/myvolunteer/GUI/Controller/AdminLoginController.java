5fd516e56a43c455d96143bc7bd50bde4f8c4bee
Kristoffermc <kristoffermchristensen@gmail.com>
2017-04-28 12:28:27 +0200

    Minor corrections

--- MyVolunteer/src/myvolunteer/GUI/Controller/AdminLoginController.java
+++ MyVolunteer/src/myvolunteer/GUI/Controller/AdminLoginController.java
@@ -13,7 +13,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import myvolunteer.GUI.Model.MainViewModel;
@@ -35,8 +34,6 @@ public class AdminLoginController implements Initializable
     private TextField UsernameField;
     @FXML
     private TextField codeField;
-    @FXML
-    private CheckBox RememberMeCB;
     @FXML
     private Button btnLogin;
 
